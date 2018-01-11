689fee488c891ed815dbe1519b8b821b5df6dc0c
Sohel Dhengre <>
2018-01-11 16:19:57 +0530

    done with final setup,installation of pods and connecting firebase

--- SocialDesk/AppDelegate.swift
+++ SocialDesk/AppDelegate.swift
@@ -3,10 +3,11 @@
 //  SocialDesk
 //
 //  Created by Sohel Dhengre on 11/01/18.
-//  Copyright © 2018 Sohel Dengre. All rights reserved.
+//  Copyright © 2018 Sohel Dhengre. All rights reserved.
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
