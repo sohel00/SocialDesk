0ba34c002d9f2245c9c96ecb1360341d2a2df59b
Sohel Dhengre <soheldhengre@gmail.com>
2018-01-18 16:00:03 +0530

    done with MeVC

---
+++ SocialDesk/Controller/MeVC.swift
@@ -0,0 +1,27 @@
+//
+//  MeVC.swift
+//  SocialDesk
+//
+//  Created by Sohel Dhengre on 18/01/18.
+//  Copyright © 2018 Sohel Dengre. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var userEmail: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func signOutPressed(_ sender: Any) {
+        
+    }
+    
+}
