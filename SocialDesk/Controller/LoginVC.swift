6270dc4f469bb01a83df6ae143b873026967b4bb
Sohel Dhengre <soheldhengre@gmail.com>
2018-01-18 15:22:42 +0530

    done with registration/login users

--- SocialDesk/Controller/LoginVC.swift
+++ SocialDesk/Controller/LoginVC.swift
@@ -15,12 +15,39 @@ class LoginVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       emailField.delegate = self
+        passwordField.delegate = self
     }
 
     @IBAction func signInPressed(_ sender: Any) {
+        if emailField.text != nil && passwordField.text != nil {
+            AuthService.instance.loginUser(withEmail: emailField.text!, andPassword: passwordField.text!, completion: { (success, loginError) in
+                if success {
+                    self.dismiss(animated: true, completion: nil)
+                } else {
+                    print(String(describing : loginError?.localizedDescription))
+                }
+                
+                AuthService.instance.registerUser(withEmail: self.emailField.text!, andPassword: self.passwordField.text!, completion: { (success, registrationError) in
+                    if success {
+                        AuthService.instance.loginUser(withEmail: self.emailField.text!, andPassword: self.passwordField.text!, completion: { (success, nil) in
+                            if success {
+                                self.dismiss(animated: true, completion: nil)
+                                print("SuccessFully Registered User")
+                            } else {
+                                print(String(describing: registrationError?.localizedDescription))
+                            }
+                        })
+                    }
+                })
+            })
+        }
+    
     }
+    
     @IBAction func closeButtonPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
 }
+
+extension LoginVC: UITextFieldDelegate{}
