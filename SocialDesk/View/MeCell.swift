ff88e0dd2978ad9ade23de451a8d6d19defcc16f
Sohel Dhengre <soheldhengre@gmail.com>
2018-03-12 16:12:17 +0530

    ui fixes

---
+++ SocialDesk/View/MeCell.swift
@@ -0,0 +1,24 @@
+//
+//  MeCell.swift
+//  SocialDesk
+//
+//  Created by Sohel Dhengre on 12/03/18.
+//  Copyright © 2018 Sohel Dengre. All rights reserved.
+//
+
+import UIKit
+
+class MeCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
